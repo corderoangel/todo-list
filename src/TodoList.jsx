6f390621982ab85a171f07f0530f940f59664a59
corderoangel <corderoan777@gmail.com>
2024-12-17 11:52:02 -0500

    components

---
+++ src/TodoList.jsx
@@ -0,0 +1,14 @@
+function TodoList() {
+	return (
+		<div>
+			<h2>TodoList</h2>
+			<ul>
+				<li>Tarea 1</li>
+				<li>Tarea 2</li>
+				<li>Tarea 3</li>
+			</ul>
+		</div>
+	);
+}
+
+export default TodoList;
